4b8d6aa8a21bdf257c8a1dedc20aca0eae56ccd7
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-02-03 20:15:03 -0300

    switch statement exercise

---
+++ src/dev/matias/devdojo/introdution/Lesson26SwitchExercise.java
@@ -0,0 +1,28 @@
+package dev.matias.devdojo.introdution;
+
+public class Lesson26SwitchExercise {
+    public static void main(String[] args) {
+        int day = 7;
+
+        if(day >= 1 && day <= 7){
+            switch (day){
+                case 1:
+                    System.out.println("its sunday");
+                    break;
+
+                case 7:
+                    System.out.println("Its saturday");
+                    break;
+
+                default:
+                    System.out.println("Its work day");
+                    break;
+            }
+        }else{
+            System.out.println("Invalid");
+        }
+
+
+
+    }
+}
