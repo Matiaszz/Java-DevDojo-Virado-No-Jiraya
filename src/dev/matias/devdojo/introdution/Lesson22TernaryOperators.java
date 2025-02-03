38f161f89a8bd4abdc08525f984f1cbc05adf9c4
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-02-03 18:43:41 -0300

    ternary operators exercise

--- src/dev/matias/devdojo/introdution/Lesson22TernaryOperators.java
+++ src/dev/matias/devdojo/introdution/Lesson22TernaryOperators.java
@@ -7,7 +7,22 @@ public class Lesson22TernaryOperators {
         String poor = "I will not donate to DevDojo";
         String result = salary >= 500 ? donate : poor;
         System.out.println(result);
+
+        // Exercise: given some salary, how much value I have to pay?
+        double annualSalary = 70000;
+        double tax;
+
+        if (annualSalary >= 68.508){
+            tax = annualSalary * 0.4950;
+            System.out.printf("Your salary is higher than €68.508, your tax is: %.2f", tax);
+
+        } else if(annualSalary >= 34.713 && annualSalary <= 68.507){
+            tax = annualSalary * 0.3735;
+            System.out.printf("Your salary is higher than €34.713 and is minor than €68.507, your tax is: %.2f", tax);
+
+        } else{
+            tax = annualSalary * 0.970;
+            System.out.printf("Your salary is minor than 34.713, your tax is: %.2f", tax);
+        }
     }
-
-
 }
