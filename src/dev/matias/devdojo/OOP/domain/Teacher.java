c993efc8738a8f665a20de81647938fb0989e85d
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-02-03 21:32:32 -0300

    OOP Exercise

---
+++ src/dev/matias/devdojo/OOP/domain/Teacher.java
@@ -0,0 +1,40 @@
+package dev.matias.devdojo.OOP.domain;
+
+import java.util.Arrays;
+
+public class Teacher {
+    private String name;
+    private int age;
+    private double[] salaries;
+
+
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public double[] getSalaries(){
+        return salaries;
+    }
+    public void setAge(int value){
+        age = value;
+    }
+    public void setName(String value){
+        name = value;
+    }
+    public void setSalary(double[] value){
+        salaries = value;
+    }
+
+    public void printData(){
+
+        System.out.println("Name: " + getName());
+        System.out.println("Age: " + getAge());
+        System.out.println("Salaries: " + Arrays.toString(getSalaries()));
+
+    }
+    public double mediaSalary(){
+        return Arrays.stream(salaries).average().orElse(0);
+    }
+}
---
+++ src/dev/matias/devdojo/OOP/tests/TestTeacher.java
@@ -0,0 +1,22 @@
+package dev.matias.devdojo.OOP.tests;
+
+
+import dev.matias.devdojo.OOP.domain.Teacher;
+
+public class TestTeacher {
+
+
+    public static void main(String[] args) {
+
+        Teacher teacher = new Teacher();
+        double[] salaries = {20, 225, 92, 230, 111};
+
+        teacher.setSalary(salaries);
+        teacher.setName("John");
+        teacher.setAge(18);
+
+        System.out.println(teacher.mediaSalary());
+
+        teacher.printData();
+    }
+}
